20060924a5ee966370892179a7cb19cb0a07aff1
spsaransh <parashar.saransh@gmail.com>
2023-08-20 23:04:48 +0530

    Array :Length of  Longest Unique SubString in an array

---
+++ array/src/LongestUniqueSubString.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestUniqueSubString {
+    public int lengthOfLongestSubstring(String s) {
+       //A,B,C,A,E,B
+        int maxLength = Integer.MIN_VALUE;
+        int ws = 0 ;
+        char[] ch = s.toCharArray();
+        int length = ch.length;
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int we = 0 ; we < length ;we++){
+            //check if character exists in the map
+            if(map.containsKey(ch[we])){
+                int charValue = map.get(ch[we]);
+                // check if this value exists in between the window
+                if(charValue >= ws && charValue<= we) {
+                    //windowstart should move to the  previous value of ch in map
+                    ws = charValue + 1;
+                }
+                //update indices
+            }
+            map.put(ch[we], we);
+            maxLength = Math.max(maxLength, we - ws +1 );
+        }
+        return  maxLength;
+    }
+}
---
+++ array/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        LongestUniqueSubString ls = new LongestUniqueSubString();
+        System.out.println(ls.lengthOfLongestSubstring("aabaab!bb"));
+    }
+}
